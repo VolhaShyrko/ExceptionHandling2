e3558d4cc7ad7c6f54fd2f733d84082e2a823b47
VolhaShyrko <olgashyrko@gmail.com>
2016-08-23 10:04:56 +0300

    first commit

---
+++ ExceptionHandling2/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExceptionHandling2
+{
+    public static class Extensions
+    {
+        public static int CustomIntParse(this string self)
+        {
+            if (string.IsNullOrEmpty(self))
+            {
+                throw new ArgumentNullException();
+            }
+            const string Digits = "0123456789";
+
+            var result = 0;
+
+            foreach (char item in self)
+            {
+                int searchResult = Digits.IndexOf(item);
+                if (searchResult == -1)
+                {
+                    throw new FormatException();
+                }
+
+                result = result * 10 + searchResult;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ExceptionHandling2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExceptionHandling2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] items = { "234", "", "This is Sparta", "1234.8", "23 " };
+
+            foreach (var item in items)
+            {
+                Console.Write("Parsing '{0}' ... ", item);
+
+                try
+                {
+                    Console.Write("Result is '{0}'", item.CustomIntParse());
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.Message);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
